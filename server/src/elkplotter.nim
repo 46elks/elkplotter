e2f7a0483ffaadc84f2d65b6e983efd7b50303a3
Rupus Reinefjord <rupus@46elks.com>
2023-05-10 11:29:32 +0200

    debug message fixes

--- server/src/elkplotter.nim
+++ server/src/elkplotter.nim
@@ -104,7 +104,7 @@ proc smsHandler(request: Request) {.gcsafe.} =
     withLock L:
       if clients.len == 0:
         request.respond(204)
-        echo "Got SMS, but no plotters connected.\nmessage: ", userPrompt
+        echo "Got SMS, but no plotters connected. Message: ", userPrompt
         return
       block findPlotter:
         for ws, pl in clients.mpairs:
@@ -112,13 +112,13 @@ proc smsHandler(request: Request) {.gcsafe.} =
             pl.isReady = false
             plotter = pl
             break findPlotter
-        echo "Got SMS, but no vacant plotters.\nmessage: ", userPrompt
+        echo "Got SMS, but no vacant plotters. Message: ", userPrompt
         request.respond(200, body = "All plotters are busy right now, try again later! :)")
         return
 
   request.respond(204)
 
-  echo "Got SMS, found vacant plotter {plotter}."
+  echo fmt"Got SMS, found vacant plotter {plotter}."
   echo "Generating image for: ", userPrompt
 
   var prompt: string
